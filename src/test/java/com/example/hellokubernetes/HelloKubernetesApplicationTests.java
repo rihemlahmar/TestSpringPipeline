2de8e9d5dfd1748576c4a2c7f178e517eb663185
rihemlahmar <rihemlahmar01@gmail.com>
2024-04-01 13:32:37 +0200

    add Dockerfiles

---
+++ src/test/java/com/example/hellokubernetes/HelloKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellokubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
